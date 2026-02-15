f7c27032ef0363b1b3e74f8ca9b0fcafc9ad2470
Dchildress4 <dchildress4@murraystate.edu>
2026-02-15 16:12:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-document.getElementById('js-test').textContent = 'JavaScript file linked'
-console.log('Script is running!')
+var button = document.getElementById("addBtn");
+var input = document.getElementById("textInput");
+var dynamic = document.getElementById("dynamic");
 
-var button = document.getElementById("addBtn")
-var input = document.getElementById("textInput")
-var dynamic = document.getElementById("dynamic")
-
-var ul = document.createElement("ul")
-dynamic.appendChild(ul)
+var ul = document.createElement("ul");
+dynamic.appendChild(ul);
 
 button.onclick = function () {
-    var li = document.createElement("li")
-    li.textContent = input.value
-    ul.appendChild(li)
-    input.value = ""
-}
+    var li = document.createElement("li");
+    li.textContent = input.value;
+    ul.appendChild(li);
+    input.value = "";
+};
