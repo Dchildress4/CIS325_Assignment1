bf3c7a0b93a6c88a53a7712e0c26d95203161481
Dchildress4 <dchildress4@murraystate.edu>
2026-02-15 16:38:33 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var button = document.getElementById("addBtn");
+var input = document.getElementById("textInput");
+var dynamic = document.getElementById("dynamic");
+
+if (button && input && dynamic) {
+    var ul = document.getElementById("myList");
+    
+    button.onclick = function () {
+        var text = input.value.trim();
+
+        if (text != "") {
+            var li = document.createElement("li");
+            li.textContent = text;
+            ul.appendChild(li);
+            input.value = "";
+        }
+    }
+};
